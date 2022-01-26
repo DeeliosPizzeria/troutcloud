d0cdf472f199f3f8d0340f9f52b26c39137b822e
JosefinDaniels <75947157+JosefinDaniels@users.noreply.github.com>
2022-01-26 15:56:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo yum update -y
+sudo pip3 install bs4
+sudo pip3 install requests
 wget https://raw.githubusercontent.com/JosefinDaniels/Trout-Cloud/main/TroutScaper.py
 sudo chmod +x TroutScaper.py
 (crontab -l; echo "* * * * * python3 /home/splunker/TroutScraper.py") | sort -u | crontab -
