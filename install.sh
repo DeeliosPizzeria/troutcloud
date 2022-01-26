62e3fb308b984b4502f578644320f7de9b4fd10e
JosefinDaniels <75947157+JosefinDaniels@users.noreply.github.com>
2022-01-26 15:46:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update -y
-wget https://github.com/JosefinDaniels/Trout-Cloud/blob/main/TroutScaper.py
+wget https://raw.githubusercontent.com/JosefinDaniels/Trout-Cloud/main/TroutScaper.py
 sudo chmod +x TroutScaper.py
 (crontab -l; echo "* * * * * python3 /home/splunker/TroutScraper.py") | sort -u | crontab -
 
